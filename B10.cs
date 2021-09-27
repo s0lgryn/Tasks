79bd4f59fcc76cecea5cadd92e3c989a03ad1490
Filipp Ginkel <69039423+s0lgryn@users.noreply.github.com>
2021-09-27 22:28:48 +0300

    Update B10.cs

--- B10.cs
+++ B10.cs
@@ -5,14 +5,35 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Console.Write("x: ");
-            int x = int.Parse(Console.ReadLine());
-            double y;
-            if (x > 0)
-                y = Math.Sin(x);
-            else
-                y = Math.Cos(x);
-            Console.WriteLine("y = " + y);
+            Console.Write("a: ");
+            double a = double.Parse(Console.ReadLine());
+            Console.Write("b: ");
+            double b = double.Parse(Console.ReadLine());
+            Console.Write("c: ");
+            double c = double.Parse(Console.ReadLine());
+            double D = b * b - 4 * a * c;
+            if (D < 0)
+                Console.WriteLine("Дискриминант меньше нуля, корней нет");
+            if (D == 0)
+            {
+                double x = (-b / (2 * a));
+				if (Math.Truncate(x) == x)
+	                Console.WriteLine("x: " + x);
+				else
+					Console.WriteLine("Корень вещественный");
+            }
+            if (D>0)
+            {
+                double x1 = ((-b - Math.Sqrt(D)) / (2 * a));
+                double x2 = ((-b + Math.Sqrt(D)) / (2 * a));
+				if((Math.Truncate(x1) == x1) || (Math.Truncate(x2) == x2))
+				{
+					Console.WriteLine("x1: " + Math.Round(x1,1));
+					Console.WriteLine("x2: " + Math.Round(x2,1));
+				}
+                else
+					Console.WriteLine("Корень вещественный");
+            }
         }
     }
 }
